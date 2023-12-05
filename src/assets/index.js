07076ec15af10113f1d3d8b78fb5753e5bbee64a
SeniboDonPedro <senibodonpedro@gmail.com>
2023-12-05 15:06:32 +0100

    one with menu

---
+++ src/assets/index.js
@@ -0,0 +1,8 @@
+import Img1 from "./1.jpg"
+import Img2 from "./2.jpg"
+import Img3 from "./3.jpg"
+import Img4 from "./4.jpg"
+import Img5 from "./5.jpg"
+import Img6 from "./6.jpg"
+
+export { Img1, Img2, Img3, Img4, Img5, Img6 }
--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,13 +1,27 @@
 import React from "react"
 import MenuCard from "../layouts/MenuCard"
+import { Img1, Img2, Img3, Img4, Img5, Img6 } from "../assets/index"
 
 const Menu = () => {
   return (
-    <section>
-      <h1>Our Menu</h1>
+    <section className="min-h-screen flex flex-col justify-center lg:px-32 px-5 bg-backgroundColor">
+      <h1 className="font-semibold text-center text-4xl mt-24 mb-8">
+        Our Menu
+      </h1>
+      <p className="text-center text-lg mb-4 mt-[-2rem] font-bold">
+        Indulge in a delightful selection of handcrafted beverages and
+        delectable treats. From classic espresso drinks to unique specialty
+        concoctions, our menu is designed to satisfy every coffee lover's
+        cravings. Explore our menu and discover your new favorite blend.
+      </p>
 
-      <div>
-        <MenuCard />
+      <div className="flex flex-wrap pb-8 gap-8 justify-center">
+        <MenuCard Img={Img1} title="Espresso" />
+        <MenuCard Img={Img2} title="Espresso" />
+        <MenuCard Img={Img3} title="Espresso" />
+        <MenuCard Img={Img4} title="Espresso" />
+        <MenuCard Img={Img5} title="Espresso" />
+        <MenuCard Img={Img6} title="Espresso" />
       </div>
     </section>
   )
